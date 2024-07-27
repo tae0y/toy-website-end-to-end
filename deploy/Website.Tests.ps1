2d77a2152c1ac372b137c03179b5d62f43a411e6
tae0y <tae0yp@gmail.com>
2024-07-27 22:04:10 +0900

    Edit workflows 프로덕션배포는 모킹만!

--- deploy/Website.Tests.ps1
+++ deploy/Website.Tests.ps1
@@ -6,18 +6,25 @@ param(
 
 Describe 'Toy Website' {
 
-    It 'Serves pages over HTTPS' {
+It 'Serves pages over HTTPS' {
       $request = [System.Net.WebRequest]::Create("https://$HostName/")
       $request.AllowAutoRedirect = $false
       $request.GetResponse().StatusCode |
         Should -Be 200 -Because "the website requires HTTPS"
     }
 
-    It 'Does not serves pages over HTTP' {
+It 'Does not serves pages over HTTP' {
       $request = [System.Net.WebRequest]::Create("http://$HostName/")
       $request.AllowAutoRedirect = $false
       $request.GetResponse().StatusCode |
         Should -BeGreaterOrEqual 300 -Because "HTTP is not secure"
+    }
+
+It 'Returns a success code from the health check endpoint' {
+      $response = Invoke-WebRequest -Uri "https://$HostName/health" -SkipHttpErrorCheck
+      Write-Host $response.Content
+      $response.StatusCode |
+        Should -Be 200 -Because "the website and configuration should be healthy"
     }
 
 }
